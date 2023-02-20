d6f4bcbfed717a86a04b2f2b7298370f37340b51
Sergio Ramirez <sergio.ramirez@thebeat.co>
2023-02-19 20:07:04 -0500

    feat: implement useError hook for JSON RPC error handling

--- marketplace/src/interfaces/index.ts
+++ marketplace/src/interfaces/index.ts
@@ -5,3 +5,11 @@ export interface IWrapped {
   provider: ethers.BrowserProvider;
   alchemy: Alchemy;
 }
+
+export interface JSONRPCError {
+  error: boolean;
+  errorCode: number | null;
+  action: string | null;
+  errorMessage: string | null;
+  method: string | null;
+}
