5030d2d2d5bd79375d39b33af770441f5e1784d3
Sergio Ramirez <sergio.ramirez@thebeat.co>
2023-02-24 01:16:18 -0500

    chore: reset nextjs to not use experimental mode

--- marketplace/app/mint-nft/page.tsx
+++
@@ -1,3 +0,0 @@
-export default () => {
-  return <div>Mint a NFT here</div>;
-};
---
+++ marketplace/pages/mint-nft/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const MintPage: NextPage = () => {
+  return <div>Mint a NFT here</div>;
+};
+
+export default MintPage;
