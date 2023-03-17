ecd0089bc26b126da34f53e6a0ce37ea9d4fbd4b
Sergio Ramirez <sergio@openloophealth.com>
2023-03-16 23:07:25 -0500

    feat: save contracts addresses on build directory

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,7 @@
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
+import path from "path";
+import fs from "fs";
 
 async function seed(nft: Contract) {
   const accounts = await ethers.getSigners();
@@ -12,6 +14,12 @@ async function seed(nft: Contract) {
 async function main() {
   const network = process.env.HARDHAT_NETWORK;
 
+  let environment: string;
+
+  if (network == "localhost") environment = "development";
+  else if (network == "mainnet") environment = "production";
+  else environment = "testing";
+
   const NFT = await ethers.getContractFactory("NFT");
   const nft = await NFT.deploy();
 
@@ -21,9 +29,29 @@ async function main() {
     nft: nft.address,
   };
 
+  const filepath = path.join(
+    __dirname,
+    `/../build/${environment}-addresses.json`
+  );
+  const dirname = path.dirname(filepath);
+
+  fs.mkdir(dirname, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    fs.writeFile(filepath, JSON.stringify(contractAddresses), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(`${environment} file written successfully`);
+    });
+  });
+
   console.log(`NFT contract deployed at ${contractAddresses.nft}`);
 
-  if (network == "localhost") await seed(nft);
+  if (environment == "development") await seed(nft);
 }
 
 main().catch((error) => {
