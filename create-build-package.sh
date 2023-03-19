fbf50a0dbddc000aee57f65e7d13d780d99ececa
Sergio Ramirez <sergio@openloophealth.com>
2023-03-18 19:25:07 -0500

    feat: add improvement to the files of the npm package

---
+++ create-build-package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if test -d "/package"; then
+    echo "package directory exists"
+else
+    mkdir package
+fi
+
+contracts=($(find ./build/contracts -name "*.sol" | xargs))
+
+echo ${#contracts[@]}
+
+counter=0
+
+while [[ $counter -lt ${#contracts[@]} ]]; do
+    # Splits the path by the '/' delimeter and passes the last element
+    # Then it splits the string by the "." to get the contract name
+    contractName=$(echo ${contracts} | cut -d"/" -f4 | cut -d"." -f1)
+
+    cp ./${contracts[counter]}/${contractName}.json ./package
+    ((counter++))
+done
