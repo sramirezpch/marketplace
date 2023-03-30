b5e2b4bb13da9e44ef4704adf5c7fc3fac162821
Sergio Ramirez <sergio@openloophealth.com>
2023-03-30 18:01:03 -0500

    chore

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721, ERC721URIStorage {
